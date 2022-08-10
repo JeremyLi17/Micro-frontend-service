607609b278c97ce636b8a280af3f725e1b5c4160
JeremyLi17 <HaotianLi0505@hotmail.com>
2022-08-10 15:29:59 -0400

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+// Mount function to start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+// If in develpmengt(iso) call mount immeditately
+if (process.env.NODE_ENV === 'development') {
+  const devRoot = document.querySelector('#_marketing-dev-root');
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+// running in container -> export the mount function
+export { mount };
